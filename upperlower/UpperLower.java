645fa95f6738957804dcf2714d4b41040272d881
Hyeonju <6841189@naver.com>
2024-07-02 08:36:43 +0900

    Create 대소문자 변환

---
+++ upperlower/UpperLower.java
@@ -0,0 +1,21 @@
+package upperlower;
+
+public class UpperLower {
+    public static void main(String[] args) {
+
+        String input = "helloWorLD";
+        char []arr;
+        arr = input.toCharArray();
+        
+        for (int i = 0; i<arr.length; i++ ){
+            if(arr[i] >= 'a' && arr[i] <= 'z'){
+                arr[i] = (char) (arr[i] + 'A' - 'a');
+            }
+            else if(arr[i] >= 'A' && arr[i] <='z'){
+                arr[i] =(char)(arr[i] -('A'-'a'));
+                
+            }
+        }
+        System.out.print(arr);
+    }
+}
