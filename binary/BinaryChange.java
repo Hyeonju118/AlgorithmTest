9a1b3eeec1f90d8274147f6c82fe9fcf93f12a24
Hyeonju <6841189@naver.com>
2024-07-02 18:10:33 +0900

    Create 이진수 변환

---
+++ binary/BinaryChange.java
@@ -0,0 +1,22 @@
+package binary;
+
+public class BinaryChange {
+    public static void main(String[] args) {
+
+        int inputNum = 19;
+        int bin[] = new int[100];
+
+        int i = 0;
+        int mod = inputNum;
+
+        while(mod > 0) {
+            bin[i] = mod % 2;
+            mod /= 2;
+            i++;
+        }
+        i--;
+        for(; i>=0; i--){
+            System.out.print(bin[i]);
+        }
+    }
+}
